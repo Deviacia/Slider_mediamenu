256195416faae4f7ad2fd81dee443e0e6e4467c0
Deviacia <ivan.bpemr@gmail.com>
2021-11-29 11:12:11 +0200

    script

--- src/script/script.js
+++ src/script/script.js
@@ -4,11 +4,15 @@
 const slider = document.querySelectorAll('.banner-section > .slide');
 const nextSlide = document.querySelector('.banner-controller__next');
 const prevSlide = document.querySelector('.banner-controller__prev');
-const slideCount = 3;
+
+const zeroSlide = 0;
+const slideCount = 2; // 0 = 1, 1 = 2, 2 = 3 ...
 
 // Get images array
 let currentSlideIndex = 0;
 let slideIndex = slider[currentSlideIndex];
+let lastEl = slider[slider - 1];
+
 
 // Check button click
 nextSlide.addEventListener('click', onChangeSlideNext);
@@ -16,15 +20,37 @@ prevSlide.addEventListener('click', onChangeSlidePrev);
 
 // Next slide
 function onChangeSlideNext() {
-    currentSlideIndex++;
     slideIndex = slider[currentSlideIndex];
+    // currentSlideIndex++;
+    
+    if (currentSlideIndex >= slideCount && currentSlideIndex <= slideCount) {
+        slideIndex = slider[0];
+        currentSlideIndex = 1;
+    }
+
+    else {
+        currentSlideIndex++;
+    }
+    
+    console.log(currentSlideIndex);
     console.log(slideIndex);
 }
 
 // Prev slide
 function onChangeSlidePrev() {
-    currentSlideIndex--;
+    // currentSlideIndex--;
     slideIndex = slider[currentSlideIndex];
+
+    if (currentSlideIndex > zeroSlide || currentSlideIndex < slideCount) {
+        currentSlideIndex--;
+    }
+
+    // else {
+    //     slideIndex = lastEl;
+    // }
+
+
+    console.log(currentSlideIndex);
     console.log(slideIndex);
 }
 
